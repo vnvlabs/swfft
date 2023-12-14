af648f791e69b8fe73dbde18c7cddecaa5c89eea
root <Ben O'Neill>
2023-12-14 16:24:30 -0500

    gitignore

--- reg_SWFFT.cpp
+++ reg_SWFFT.cpp
@@ -1,2 +1,22 @@
+//8467656702857937604
+/// This file was automatically generated using the VnV-Matcher executable. 
+/// The matcher allows for automatic registration of all VnV plugins and injection 
+/// points. Building the matcher requires Clang. If Clang is not available on this machine,
+/// Registration code should be written manually. 
+/// 
+
+//PACKAGENAME: SWFFT
+
 #include "VnV.h" 
-INJECTION_REGISTRATION(SWFFT){}
+const char* getFullRegistrationJson_SWFFT(){
+	 return "{\"Conclusion\":{\"configuration\":{},\"description\":\"\",\"instructions\":\"\",\"params\":{},\"shortTitle\":\"\",\"template\":\"\",\"title\":\"\"},\"InjectionPoints\":{\"forward\":{\"docs\":{\"configuration\":{},\"description\":\"\",\"instructions\":\"\",\"params\":{},\"shortTitle\":\"\",\"template\":\"\",\"title\":\"\"},\"loop\":true,\"name\":\"forward\",\"packageName\":\"SWFFT\",\"parameters\":{\"void hacc::Dfft::forward(const complex_t *)\":{\"start\":\"double\"}},\"stages\":{\"Begin\":{\"docs\":{\"configuration\":{},\"description\":\"\",\"instructions\":\"\",\"params\":{},\"shortTitle\":\"\",\"template\":\"\",\"title\":\"\"},\"info\":{\"Calling Function\":\"forward\",\"Calling Function Column\":3,\"Calling Function Line\":146,\"filename\":\"./Dfft.hpp\",\"lineColumn\":5,\"lineNumber\":65}},\"End\":{\"info\":{\"Calling Function\":\"forward\",\"Calling Function Column\":3,\"Calling Function Line\":146,\"filename\":\"./Dfft.hpp\",\"lineColumn\":5,\"lineNumber\":74}}}}},\"Introduction\":{\"configuration\":{},\"description\":\"\",\"instructions\":\"\",\"params\":{},\"shortTitle\":\"\",\"template\":\"\",\"title\":\"\"}}";}
+
+INJECTION_REGISTRATION(SWFFT){
+	Register_Injection_Point("SWFFT","forward","{\"void hacc::Dfft::forward(const complex_t *)\":{\"start\":\"double\"}}");
+	REGISTER_FULL_JSON(SWFFT, getFullRegistrationJson_SWFFT);
+}
+
+
+
+FORTRAN_INJECTION_REGISTRATION(SWFFT)
+
--- reg_TestDFFT.cpp
+++ reg_TestDFFT.cpp
@@ -1,3 +1,23 @@
-#include "VnV.h" 
+//5770155309620627691
+/// This file was automatically generated using the VnV-Matcher executable. 
+/// The matcher allows for automatic registration of all VnV plugins and injection 
+/// points. Building the matcher requires Clang. If Clang is not available on this machine,
+/// Registration code should be written manually. 
+/// 
 
-INJECTION_REGISTRATION(TestDFFT){}
+//PACKAGENAME: TestDFFT
+
+#include "VnV.h" 
+DECLARESUBPACKAGE(SWFFT)
+const char* getFullRegistrationJson_TestDFFT(){
+	 return "{\"Executables\":{\"configuration\":{},\"default\":{},\"description\":\"\",\"instructions\":\"\",\"lib\":\"executables\",\"params\":{},\"shortTitle\":\"\",\"template\":\"\\n \\n \\n\",\"title\":\"Test DFFT Application\"},\"SubPackages\":{\"SWFFT\":{\"docs\":{\"configuration\":{},\"description\":\"\",\"instructions\":\"\",\"params\":{},\"shortTitle\":\"\",\"template\":\"\",\"title\":\"\"},\"name\":\"SWFFT\",\"packageName\":\"TestDFFT\"}}}";}
+
+INJECTION_REGISTRATION(TestDFFT){
+	REGISTERSUBPACKAGE(SWFFT);
+	REGISTER_FULL_JSON(TestDFFT, getFullRegistrationJson_TestDFFT);
+}
+
+
+
+FORTRAN_INJECTION_REGISTRATION(TestDFFT)
+
